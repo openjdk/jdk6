e61c8ce8fba1fc01fe2991fa1fcb9c204716967d
Weijun Wang <weijun@openjdk.org>
2016-12-05 04:15:08 +0000

    6923681: Jarsigner crashes during timestamping
    Reviewed-by: vinnie

--- jdk/src/share/classes/sun/security/tools/jarsigner/TimestampedSigner.java
+++ jdk/src/share/classes/sun/security/tools/jarsigner/TimestampedSigner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -376,7 +376,8 @@ public final class TimestampedSigner extends ContentSigner {
                 }
                 if (!isSigner) {
                     keyPurposes = cert.getExtendedKeyUsage();
-                    if (! keyPurposes.contains(KP_TIMESTAMPING_OID)) {
+                    if (keyPurposes == null ||
+                            ! keyPurposes.contains(KP_TIMESTAMPING_OID)) {
                         throw new CertificateException(
                             "Certificate is not valid for timestamping");
                     }
