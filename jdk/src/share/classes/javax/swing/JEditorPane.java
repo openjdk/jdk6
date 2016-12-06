a6444973f420a7909953a8b0acbc2bb390a4784e
Mikhail Cherkasov <mcherkas@openjdk.org>
2016-12-06 12:57:38 +0000

    8158734: JEditorPane.createEditorKitForContentType throws NPE after 6882559
    Reviewed-by: serb, aivanov

--- jdk/src/share/classes/javax/swing/JEditorPane.java
+++ jdk/src/share/classes/javax/swing/JEditorPane.java
@@ -1242,7 +1242,7 @@ public class JEditorPane extends JTextComponent {
         if (k == null) {
             // try to dynamically load the support
             String classname = getKitTypeRegistry().get(type);
-            ClassLoader loader = getKitLoaderRegistry().get(type).orElse(null);
+            ClassLoader loader = getKitLoaderRegistry().get(type);
             try {
                 Class c;
                 if (loader != null) {
@@ -1299,7 +1299,11 @@ public class JEditorPane extends JTextComponent {
      */
     public static void registerEditorKitForContentType(String type, String classname, ClassLoader loader) {
         getKitTypeRegistry().put(type, classname);
-        getKitLoaderRegistry().put(type, Optional.ofNullable(loader));
+        if (loader != null) {
+            getKitLoaderRegistry().put(type, loader);
+        } else {
+            getKitLoaderRegistry().remove(type);
+        }
         getKitRegisty().remove(type);
     }
 
@@ -1320,7 +1324,7 @@ public class JEditorPane extends JTextComponent {
         return (Hashtable)SwingUtilities.appContextGet(kitTypeRegistryKey);
     }
 
-    private static Hashtable<String, Optional<ClassLoader>> getKitLoaderRegistry() {
+    private static Hashtable<String, ClassLoader> getKitLoaderRegistry() {
         loadDefaultKitsIfNecessary();
         return (Hashtable)SwingUtilities.appContextGet(kitLoaderRegistryKey);
     }
@@ -2464,132 +2468,5 @@ static class HeaderParser {
         }
     }
  }
-
-    private static final class Optional<T> {
-        /**
-         * Common instance for {@code empty()}.
-         */
-        private static final Optional<?> EMPTY = new Optional<Object>();
-
-        /**
-         * If non-null, the value; if null, indicates no value is present
-         */
-        private final T value;
-
-        /**
-         * Constructs an empty instance.
-         *
-         * @implNote Generally only one empty instance, {@link Optional#EMPTY},
-         * should exist per VM.
-         */
-        private Optional() {
-            this.value = null;
-        }
-
-        /**
-         * Returns an empty {@code Optional} instance.  No value is present for this
-         * Optional.
-         *
-         * @apiNote Though it may be tempting to do so, avoid testing if an object
-         * is empty by comparing with {@code ==} against instances returned by
-         * {@code Option.empty()}. There is no guarantee that it is a singleton.
-         * Instead, use {@link #isPresent()}.
-         *
-         * @param <T> Type of the non-existent value
-         * @return an empty {@code Optional}
-         */
-        public static<T> Optional<T> empty() {
-            @SuppressWarnings("unchecked")
-                Optional<T> t = (Optional<T>) EMPTY;
-            return t;
-        }
-
-        /**
-         * Constructs an instance with the value present.
-         *
-         * @param value the non-null value to be present
-         * @throws NullPointerException if value is null
-         */
-        private Optional(T value) {
-            this.value = requireNonNull(value);
-        }
-
-        /**
-         * Returns an {@code Optional} with the specified present non-null value.
-         *
-         * @param <T> the class of the value
-         * @param value the value to be present, which must be non-null
-         * @return an {@code Optional} with the value present
-         * @throws NullPointerException if value is null
-         */
-        public static <T> Optional<T> of(T value) {
-            return new Optional<T>(value);
-        }
-
-        /**
-         * Returns an {@code Optional} describing the specified value, if non-null,
-         * otherwise returns an empty {@code Optional}.
-         *
-         * @param <T> the class of the value
-         * @param value the possibly-null value to describe
-         * @return an {@code Optional} with a present value if the specified value
-         * is non-null, otherwise an empty {@code Optional}
-         */
-        public static <T> Optional<T> ofNullable(T value) {
-            if (value == null) {
-                return empty();
-            } else {
-                return of(value);
-            }
-        }
-
-        /**
-         * If a value is present in this {@code Optional}, returns the value,
-         * otherwise throws {@code NoSuchElementException}.
-         *
-         * @return the non-null value held by this {@code Optional}
-         * @throws NoSuchElementException if there is no value present
-         *
-         * @see Optional#isPresent()
-         */
-        public T get() {
-            if (value == null) {
-                throw new NoSuchElementException("No value present");
-            }
-            return value;
-        }
-
-        /**
-         * Return the value if present, otherwise return {@code other}.
-         *
-         * @param other the value to be returned if there is no value present, may
-         * be null
-         * @return the value, if present, otherwise {@code other}
-         */
-        public T orElse(T other) {
-            return value != null ? value : other;
-        }
-
-        /**
-	 * Checks that the specified object reference is not {@code null}. This
-	 * method is designed primarily for doing parameter validation in methods
-	 * and constructors, as demonstrated below:
-	 * <blockquote><pre>
-	 * public Foo(Bar bar) {
-	 *     this.bar = Objects.requireNonNull(bar);
-	 * }
-	 * </pre></blockquote>
-	 *
-	 * @param obj the object reference to check for nullity
-	 * @param <T> the type of the reference
-	 * @return {@code obj} if not {@code null}
-	 * @throws NullPointerException if {@code obj} is {@code null}
-	 */
-	private static <T> T requireNonNull(T obj) {
-	    if (obj == null)
-		throw new NullPointerException();
-	    return obj;
-	}
-    }
 
 }
