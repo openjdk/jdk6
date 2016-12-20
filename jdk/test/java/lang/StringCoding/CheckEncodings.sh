f5ca8567c16d48752096469fae9c5252ad8d8497
Kelly O'Hair <ohair@openjdk.org>
2016-12-20 03:34:40 +0000

    6911129: These tests do not work with CYGWIN: java/lang
    Reviewed-by: tbell, alanb

--- jdk/test/java/lang/StringCoding/CheckEncodings.sh
+++ jdk/test/java/lang/StringCoding/CheckEncodings.sh
@@ -31,7 +31,7 @@
 OS=`uname -s`
 case "$OS" in
   SunOS | Linux | Darwin | AIX ) ;;
-  Windows* )
+  Windows* | CYGWIN* )
     echo "Passed"; exit 0 ;;
   * ) echo "Unrecognized system!" ;  exit 1 ;;
 esac
