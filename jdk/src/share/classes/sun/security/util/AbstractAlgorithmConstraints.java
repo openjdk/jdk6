2a3943c530c1b25fb5d117bb6526d4e2d94950c5
Andrew John Hughes <andrew@openjdk.org>
2017-01-01 04:19:08 +0000

    8172159: Remove @Override annotation on interfaces added by b41 updates
    Reviewed-by: omajid

--- jdk/src/share/classes/sun/security/util/AbstractAlgorithmConstraints.java
+++ jdk/src/share/classes/sun/security/util/AbstractAlgorithmConstraints.java
@@ -48,7 +48,6 @@ public abstract class AbstractAlgorithmConstraints
             final String propertyName) {
         String property = AccessController.doPrivileged(
             new PrivilegedAction<String>() {
-                @Override
                 public String run() {
                     return Security.getProperty(propertyName);
                 }
