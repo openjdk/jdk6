fcfb7b0c1bd1998f1c97ff8132b4cb75b4ab07e3
Sean Coffey <coffeys@openjdk.org>
2017-09-26 07:11:37 -0700

    8170157: Enable unlimited cryptographic policy by default in OracleJDK
    Reviewed-by: wetmore

--- jdk/test/javax/crypto/CryptoPermission/TestUnlimited.java
+++ jdk/test/javax/crypto/CryptoPermission/TestUnlimited.java
@@ -25,12 +25,13 @@
 
 /**
  * @test
- * @bug 8157561
- * @summary Ship the unlimited policy files in JDK Updates
+ * @bug 8061842
+ * @summary Package jurisdiction policy files as something other than JAR
+ * @run main/othervm TestUnlimited use_default default
  * @run main/othervm TestUnlimited "" exception
- * @run main/othervm TestUnlimited limited fail
- * @run main/othervm TestUnlimited unlimited pass
- * @run main/othervm TestUnlimited unlimited/ pass
+ * @run main/othervm TestUnlimited limited limited
+ * @run main/othervm TestUnlimited unlimited unlimited
+ * @run main/othervm TestUnlimited unlimited/ exception
  * @run main/othervm TestUnlimited NosuchDir exception
  * @run main/othervm TestUnlimited . exception
  * @run main/othervm TestUnlimited /tmp/unlimited exception
@@ -39,9 +40,39 @@
  * @run main/othervm TestUnlimited /unlimited exception
  */
 import javax.crypto.*;
+import java.io.*;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestUnlimited {
+
+    private enum Result {
+        UNLIMITED,
+        LIMITED,
+        EXCEPTION,
+        UNKNOWN
+    };
+
+    /*
+     * Grab the default policy entry from java.security.
+     *
+     * If the input java.security file is malformed
+     * (missing crypto.policy, attribute/no value, etc), throw
+     * exception.  split() might throw AIOOB which
+     * is ok behavior.
+     */
+    private static String getDefaultPolicy() throws Exception {
+        String path = System.getProperty("java.home") + "/lib/security/java.security";
+        List<String> lines = readAllLines(new File(path));
+
+        for (String line : lines) {
+            if (line.startsWith("crypto.policy=")) {
+                return line.split("=")[1].trim();
+            }
+        }
+        return "";
+    }
 
     public static void main(String[] args) throws Exception {
         /*
@@ -53,16 +84,36 @@ public class TestUnlimited {
             throw new Exception("Two args required");
         }
 
-        boolean expected = args[1].equals("pass");
-        boolean exception = args[1].equals("exception");
-        boolean result = false;
+        String testStr = args[0];
+        String expectedStr = args[1];
+        if (testStr.equals("use_default")) {
+            expectedStr = getDefaultPolicy();
+        }
 
-        System.out.println("Testing: " + args[0]);
+        Result expected = Result.UNKNOWN;  // avoid NPE warnings
+        Result result;
 
-        if (args[0].equals("\"\"")) {
+        if (expectedStr.equals("") || expectedStr.equals("unlimited")) {
+            expected = Result.UNLIMITED;
+
+        } else if (expectedStr.equals("limited")) {
+            expected = Result.LIMITED;
+
+        } else if (expectedStr.equals("exception")) {
+            expected = Result.EXCEPTION;
+
+        } else {
+            throw new Exception("Unexpected argument");
+        }
+
+        System.out.println("Testing: " + testStr);
+        if (testStr.equals("\"\"")) {
             Security.setProperty("crypto.policy", "");
         } else {
-            Security.setProperty("crypto.policy", args[0]);
+            // skip default case.
+            if (!testStr.equals("use_default")) {
+                Security.setProperty("crypto.policy", testStr);
+            }
         }
 
         /*
@@ -74,23 +125,34 @@ public class TestUnlimited {
             System.out.println("max AES key len:" + maxKeyLen);
             if (maxKeyLen > 128) {
                 System.out.println("Unlimited policy is active");
-                result = true;
+                result = Result.UNLIMITED;
             } else {
                 System.out.println("Unlimited policy is NOT active");
-                result = false;
+                result = Result.LIMITED;
             }
         } catch (Throwable e) {
-            if (!exception) {
-                throw new Exception();
-            }
+            //ExceptionInInitializerError's
+            result = Result.EXCEPTION;
         }
 
         System.out.println(
                 "Expected:\t" + expected + "\nResult:\t\t" + result);
-        if (expected != result) {
-            throw new Exception();
+        if (!expected.equals(result)) {
+            throw new Exception("Didn't match");
         }
 
         System.out.println("DONE!");
     }
+
+    private static List<String> readAllLines(File path) throws IOException {
+        List<String> strings = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        String line = reader.readLine();
+        while (line != null) {
+            strings.add(line);
+            line = reader.readLine();
+        }
+        return strings;
+    }
+
 }
